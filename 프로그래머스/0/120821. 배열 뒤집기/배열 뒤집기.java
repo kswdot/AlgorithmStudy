02e10adf536d82eeda87b9776d301402a6e68370
Seongeun Kim <102601303+kswdot@users.noreply.github.com>
2025-08-17 19:38:08 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int temp = 0;
+        
+        for (int i = 0; i < num_list.length / 2; i++) {
+            temp = num_list[num_list.length - i - 1];
+            num_list[num_list.length - i - 1] = num_list[i];
+            num_list[i] = temp;
+        }
+        
+        return num_list;
+    }
+}
