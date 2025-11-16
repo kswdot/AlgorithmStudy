9e8064e47efa0743fe8b6f08e60e70b7d9b17f47
Seongeun Kim <102601303+kswdot@users.noreply.github.com>
2025-11-16 16:54:08 +0900

    [Silver IV] Title: 2+1 세일, Time: 408 ms, Memory: 27596 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[num];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 내림차순 정렬
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        int sum = 0;
+
+        // 3번째 인덱스는 합계에서 제외
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 3 != 2) {
+                sum += arr[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
