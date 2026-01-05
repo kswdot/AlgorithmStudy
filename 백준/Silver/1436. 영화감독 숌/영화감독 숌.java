77e81ed5572479b1a2c85f7438b29fb6a025ed30
Seongeun Kim <102601303+kswdot@users.noreply.github.com>
2026-01-06 00:48:35 +0900

    [Silver V] Title: 영화감독 숌, Time: 328 ms, Memory: 88808 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// 문제: 영화감독 숌 (실버 5)
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int count = 0;
+        int num = 666;
+
+        while (true) {
+            if (String.valueOf(num).contains("666")) {
+                count++;
+                if (count == k) {
+                    System.out.println(num);
+                    break;
+                }
+            }
+            num++;
+        }
+    }
+}
