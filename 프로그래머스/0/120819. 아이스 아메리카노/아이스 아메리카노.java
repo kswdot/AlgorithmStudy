635d1d089ef6f876525ee5204262f856f3c11c3d
Seongeun Kim <102601303+kswdot@users.noreply.github.com>
2026-06-08 18:18:43 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,18 +1,5 @@
 class Solution {
     public int[] solution(int money) {
-        int[] answer = new int[2];
-        
-        // money: 가지고 있는 돈, 아아 = 5500
-        // return: 최대로 마실수 있는 잔 수, 남는 돈
-        // 반복문 사용
-        for (int i = 1; i <= 200; i++) {
-            if (i * 5500 > money) {
-                answer[0] = i - 1;
-                answer[1] = money - ((i - 1) * 5500);
-                break;
-            }
-        }
-        
-        return answer;
+        return new int[] {money / 5500, money % 5500};
     }
 }
