b858cf85af1024e2626af06168409cb7eea5d0f7
Seongeun Kim <102601303+kswdot@users.noreply.github.com>
2026-06-17 21:31:31 +0900

    [level 2] Title: 올바른 괄호, Time: 10.41 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 54.9 MB, 시간: 20.38 ms
+메모리: 57.7 MB, 시간: 10.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 25일 19:39:28
+2026년 06월 17일 21:31:27
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,25 +2,35 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
+        
         boolean answer = true;
-
+        
+        // 스택 선언 및 초기화
         Stack<Character> stack = new Stack<>();
         
+        // '(' 라면 스택 쌓기
+        // 만약 ')' 이고 스택의 마지막이 '(' 라면 스택 꺼내기
+        // s가 끝났는데 스택이 비어있지 않다면 false 반환
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            // s의 인덱스가 여는 괄호면 stack.push
-            if (c == '(') {
-                stack.push(c);
-                // s의 인덱스가 닫는 괄호면 스택에 문자열이 있는지 없는지 확인
-            } else if (c == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                } else if (stack.pop() == 'c') {
-                    return false;
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            }
+            else {
+                if (stack.isEmpty() == true) {
+                    answer = false;
+                }
+                else if (stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    answer = false;
                 }
             }
         }
         
-        return stack.isEmpty();
+        if (stack.isEmpty() == false) {
+            answer = false;
+        }
+        
+        return answer;
     }
 }
