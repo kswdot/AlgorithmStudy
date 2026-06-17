e7554cdf53662c818c9adea625bd189f01c4668a
Seongeun Kim <102601303+kswdot@users.noreply.github.com>
2026-06-17 22:26:20 +0900

    [level 2] Title: 프로세스, Time: 2.09 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,51 +1,46 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-    // 프로세스 클래스 생성
-    class Process {
-        int index;    // 위치
-        int priority; // 우선순위
-
-        Process(int index, int priority) {
-            this.index = index;
-            this.priority = priority;
-        }
-    }
-
     public int solution(int[] priorities, int location) {
-        Queue<Process> queue = new LinkedList<>();
+        // 큐 생성 및 int[현재 위치 인덱스, 중요도 순위] 배열 저장
+        Queue<int[]> queue = new LinkedList<>();
         
         for (int i = 0; i < priorities.length; i++) {
-            queue.add(new Process(i, priorities[i]));
+            queue.add(new int[]{i, priorities[i]});
         }
-
-        int turn = 0; // 실행 순서
-
+        
+        // 현재 실행 순서 저장 변수
+        int order = 0;
+        
+        // 큐가 빌 때까지 실행
         while (!queue.isEmpty()) {
-            Process current = queue.poll(); 
-            boolean hasHigherPriority = false;
-
+            // 맨 앞 프로세스 저장 변수
+            int[] current = queue.poll();
+            // 뒤에 중요도가 더 높은 프로세스가 있는지 저장 변수
+            boolean higher = false;
             
-            for (Process p : queue) {
-                if (p.priority > current.priority) {
-                    hasHigherPriority = true;
+            // 뒤에 중요도가 더 높은 프로세스가 있는지 확인
+            for (int[] next : queue) {
+                if (current[1] < next[1]) {
+                    higher = true;
                     break;
                 }
             }
-
-            // 우선 순위가 높으면 뒤로
-            if (hasHigherPriority) {
+            
+            // 만약 뒤에 중요도가 더 높은 프로세스가 있다면 큐에 다시 저장
+            if (higher) {
                 queue.add(current);
             } else {
-                turn++;
+                // 현재 프로세스가 중요도가 제일 높다면 실행
+                order++;
                 
-                if (current.index == location) {
-                    return turn;
-                }
+                // 변수로 주어진 location과 현재 위치 인덱스가 같다면 해당 순서 반환
+                if (location == current[0]) {
+                    return order;
+                } 
             }
         }
-
-        return turn;
+        
+        return order;
     }
 }
