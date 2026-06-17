9319add64eccdc2f85f58c78f44cd675af525f04
Seongeun Kim <102601303+kswdot@users.noreply.github.com>
2026-06-17 20:47:30 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 87.2 MB, 시간: 4.14 ms
+메모리: 78.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 01일 15:17:45
+2026년 06월 17일 20:47:27
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,33 +2,51 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new ArrayDeque<>();
         
-        // 남은 일 수를 담을 배열 객체 생성
+        // 앞 작업이 완료될 때까지 뒷 작업은 완료되지 못함
+        // 남은 일수 계산해서 저장
         int[] leftDays = new int[progresses.length];
         
-        // 남은 일 수 계산해서 배열에 넣기
         for (int i = 0; i < progresses.length; i++) {
-            leftDays[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+            // 남은 작업량
+            int remainProgress = 100 - progresses[i];
+            int days = remainProgress / speeds[i];
+            
+            // 나머지가 있으면 하루 더 필요
+            if (remainProgress % speeds[i] != 0) {
+                days++;
+            }
+            
+            leftDays[i] = days;
         }
         
-        // 최대 배포일 설정
-        // progresses의 첫 번째 인덱스를 배포 가능일로 설정
-        int maxDay = leftDays[0];
-        int count = 0;
+        ArrayList<Integer> list = new ArrayList<>();
         
-        // 배포될 progresses의 개수를 큐에 넣기
-        for (int i = 0; i < progresses.length; i++) {
+        // 첫 번째 작업의 남은 일수를 기준일로
+        int maxDay = leftDays[0];
+        int count = 1;
+        
+        // 맨 앞 인덱스와 비교해서 맨 앞 인덱스보다 작거나 같은 값 카운트
+        for (int i = 1; i < leftDays.length; i++) {
             if (leftDays[i] <= maxDay) {
                 count++;
             } else {
-                queue.add(count);
-                count = 1;
+                // 기준일보다 오래 걸릴 경우 끊고 기준일 다시 설정
+                list.add(count);
                 maxDay = leftDays[i];
+                count = 1;
             }
         }
         
-        queue.add(count);
-        return queue.stream().mapToInt(Integer::intValue).toArray();
+        list.add(count);
+        
+        // 리스트 -> 배열
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        
+        return answer;
     }
 }
